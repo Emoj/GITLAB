17c9ee2995bd966ef130e909fcf39ad6a73dfb04
owlaaz <owlaaz@outlook.com>
2017-04-25 17:02:27 +0700

    modified Mmc_Fat16_Test.c

--- Mmc_Fat16_Test.c
+++ Mmc_Fat16_Test.c
@@ -191,9 +191,9 @@ void M_Create_Swap_File() {
 void main() {
      // we will use PORTC to signal test end
      DDRC  = 0xFF;
-     PORTC = 0xFF;  
+     PORTC = 0xF0;  
      //--- set up USART for the file read
-     UART1_Init(9600);
+     UART1_Init(4800);
      Delay_100ms();
      //--- init the FAT library
      SPI1_Init_Advanced(_SPI_MASTER, _SPI_FCY_DIV128, _SPI_CLK_LO_LEADING);
